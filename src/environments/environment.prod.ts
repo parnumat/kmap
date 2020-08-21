bfbda20c0123ece1f8a7cbf7febf2ded7e75e5c4
porntip-ingeni <porntip.ingeni@gmail.com>
2020-08-21 16:27:41 +0700

    ...

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://192.168.55.53:1189/api/',
+  apiUrl: 'https://portal.kimpai.com/dotnetcore/kmapBack-end/',
   // apiUrl: 'http://localhost:5000/api/',
   whitelist: ['192.168.55.53:1189', '192.168.55.76'],
   blacklisted: ['192.168.55.53:1189/api','192.168.55.76/ToppWebApi/api/auth']
